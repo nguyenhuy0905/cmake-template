a0ee11d1bac46468ecfbf88c173d99c5a321b4e6
Huy Nguyen <huy55465@gmail.com>
2025-01-30 15:39:36 -0500

    change workflow again

--- conanfile.py
+++ conanfile.py
@@ -9,10 +9,10 @@ class Recipe(ConanFile):
 
     def requirements(self):
         self.requires("spdlog/1.15.0")
-        self.test_requires("catch2/3.8.0")
         # you probably will like these
         # self.requires("asio/1.32.0")
         # self.requires("nlohmann_json/3.11.3")
         # self.requires("raylib/5.5")
 
-    # def build_requirements(self):
+    def build_requirements(self):
+        self.test_requires("catch2/3.8.0")
